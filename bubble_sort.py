3ffec505c58f709db4cf12dc5c01d80b67d51ee6
Yadav <tulbendra.yadav@capgemini.com>
2024-08-29 12:42:06 +0530

    Added new sorting mechanism

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,22 @@
 
 def bubble_sort(arr):
-
+    """This is a code to sort array by bubble sort mechanism"""
     for i in range(1, len(arr)+1):
+        y = i
         for idx in range(len(arr) - i):
             if arr[idx] > arr[idx+1]:
                 arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
+
+
+def insertion_sort(arr):
+    """This is to sort an array by insertion sort"""
+    for idx, item in enumerate(arr):
+        position = idx
+        y = position
+        while position > 0 and item < arr[position - 1]:
+            arr[position], arr[position - 1] = arr[position - 1], arr[position]
+            position = position - 1
+    return arr
 
 
 if __name__ == "__main__":
